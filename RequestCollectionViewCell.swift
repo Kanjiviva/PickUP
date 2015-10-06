37a661aa0a68730ca0b4955f2716121facbcaa63
Derrick Park <derrick8689@gmail.com>
2015-10-05 18:02:39 -0700

    add details to collection view controller

---
+++ RequestCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RequestCollectionViewCell.swift
+//  PickUP
+//
+//  Created by Derrick Park on 2015-10-05.
+//  Copyright © 2015 Steve. All rights reserved.
+//
+
+import UIKit
+
+class RequestCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ RequestsHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  RequestsHeaderView.swift
+//  PickUP
+//
+//  Created by Derrick Park on 2015-10-05.
+//  Copyright © 2015 Steve. All rights reserved.
+//
+
+import UIKit
+
+class RequestsHeaderView: UICollectionReusableView {
+        
+}
